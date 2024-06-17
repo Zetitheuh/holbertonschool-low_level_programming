b58ba25e95cb447df2f5014f6ab480f93014af2d
Zetitheuh <zetithondev@gmail.com>
2024-06-17 15:36:40 +0200

    s

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main(void) /*Return a simple phrase */
+/**
+ * Print a simple phrase
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
