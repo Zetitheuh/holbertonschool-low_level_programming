807c9102d11acc2c66471feadb56329321fc25cb
Zetitheuh <zetithondev@gmail.com>
2024-06-17 16:26:26 +0200

    s

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main (void)
 {
-	printf("Size of a char"\n, sizeof(char));
-	printf("Size of an int"\n, sizeof(int));
-	printf("Size of long int"\n, sizeof(long int));
-	printf("Size of a long long int"\n, sizeof(long int));
+	printf("Size of a char\n", sizeof(char));
+	printf("Size of an int\n", sizeof(int));
+	printf("Size of long int\n", sizeof(long int));
+	printf("Size of a long long int\n", sizeof(long int));
 	printf("Size of a float\n", sizeof(float));	
 	return (0);
 }
