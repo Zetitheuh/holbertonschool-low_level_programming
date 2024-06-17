514e3d6b814499736aa95e5c04a2d23e1345685d
Zetitheuh <zetithondev@gmail.com>
2024-06-17 15:51:11 +0200

    s

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdlib.h>
+
+/**
+ * main - Entry of the program
+ * this functions print a message to the console using printf
+ *
+ * Return: Zero = The program was successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
