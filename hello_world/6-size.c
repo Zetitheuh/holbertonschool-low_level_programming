33e42e8752e7ae4907321f0fb21bd169f76432f4
Zetitheuh <zetithondev@gmail.com>
2024-06-17 16:25:03 +0200

    s

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ * this functions print many exemple of how many bytes are differents types
+ */
+int main (void)
+{
+	printf("Size of a char"\n, sizeof(char));
+	printf("Size of an int"\n, sizeof(int));
+	printf("Size of long int"\n, sizeof(long int));
+	printf("Size of a long long int"\n, sizeof(long int));
+	printf("Size of a float\n", sizeof(float));	
+	return (0);
+}
