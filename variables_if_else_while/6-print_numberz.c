2f84f3b12425fa3f31d2488d9decd0200d48c627
Zetitheuh <zetithondev@gmail.com>
2024-06-18 15:27:20 +0200

    s

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry of the program
+ *  print all digit number with putchar
+ *
+ *  Return: Zero if program well executed
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i + '0');
+return (0);
+}
