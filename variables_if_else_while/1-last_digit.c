6c81a190e599b5c83b7d81e2745a0ac96b34c71f
Zetitheuh <zetithondev@gmail.com>
2024-06-18 12:26:19 +0200

    s

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -5,10 +5,10 @@
 
 /* betty style doc for function main goes there */
 /**
- *
  * main - Entry point of the program
- *
  * this function call a random number and compare the last number
+ *
+ * Return: Zero = welle executed
  */
 int main(void)
 {
@@ -19,14 +19,14 @@ int main(void)
 	int last_number = n % 10;
 	if (last_number == 0)
 	{
-		printf("Last digit of %d is %d  and is 0",n, last_number);
+		printf("Last digit of %d is %d  and is 0", n, last_number);
 	}
 	else if (last_number != 0 && last_number < 6)
 	{
-		printf("Last digit of %d is %d  and is less than 6 and not 0",n, last_number);
+		printf("Last digit of %d is %d  and is less than 6 and not 0", n, last_number);
 	}
 	else if (last_number > 5)
 	{
-		printf("Last digit of %d is  %d  is and is greater than 5",n, last_number);
+		printf("Last digit of %d is  %d  is and is greater than 5", n, last_number);
 	} 
 	return (0);
