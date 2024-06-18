45f84e334bc81143588cb6aa7f13ffc6039dbd43
Zetitheuh <zetithondev@gmail.com>
2024-06-18 14:22:34 +0200

    s

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry of the progrq;
+ * Description of what do
+ *
+ * Return: Program well executed
+ **/
+int main(void)
+{
+        char letter;
+
+        for (letter = 'a'; letter <= 'Z'; letter++)
+                putchar(letter);
+        putchar('\n');
+        return (0);
+}
+
