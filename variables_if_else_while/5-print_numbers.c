84d21d1a2d687d1ffc5e12a81601dcefccd31d72
Zetitheuh <zetithondev@gmail.com>
2024-06-18 14:59:27 +0200

    s

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry of programm
+ * print number from 0 to 10
+ *
+ * Return: Programm work
+ */
+int main(void)
+{
+	int i
+	for (i = 0; i <= 9; i++)
+		printf("%d\n", i)
+return (0);
+}
