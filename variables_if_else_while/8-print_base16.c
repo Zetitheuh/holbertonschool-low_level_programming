2c0bd278d3bb2ac307d754ef7810e72eec435a7e
Zetitheuh <zetithondev@gmail.com>
2024-06-18 23:27:04 +0200

    w

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,11 +8,10 @@
 int main(void)
 {
 	char i;
-	int f = 5
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 		putchar(i);
-	for (i = a; i <= f; i++)
+	for (i = 'a'; i <= 'f'; i++)
 		putchar(i);
 	putchar('\n');
 return (0);
