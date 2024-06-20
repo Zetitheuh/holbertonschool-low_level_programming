4c41f66847db54ea50f0aab5031f9259ad3b9406
Zetitheuh <zetithondev@gmail.com>
2024-06-20 22:06:22 +0200

    ss

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
+ * print_alphabet - function mes couilles
  * print the alphabet X10
  */
 void print_alphabet_x10(void)
 {	
 	int compteur = 0;
 
-	while (compteur <= 10)
+	while (compteur < 10)
 	{
 		print_alphabet();
+		compteur++;
 	}
 
 	_putchar('\n');
