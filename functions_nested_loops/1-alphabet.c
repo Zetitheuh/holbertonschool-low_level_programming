c7a9a1e168d9d0a5efcdacb4a2f07befe4c14379
Zetitheuh <zetithondev@gmail.com>
2024-06-20 15:23:52 +0200

    s

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    void print_alphabet(void)
+    {
+	    char lettre;
+
+	    for(lettre = 'a'; lettre <= 'z'; lettre++)
+	    {
+		    _putchar(lettre);
+	    }
+    }
+    return (0);
+}
