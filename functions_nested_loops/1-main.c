097bf83726103d78aa29e86397448dc15906f690
Zetitheuh <zetithondev@gmail.com>
2024-06-20 20:51:03 +0200

    s

--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
-void print_alphabet(void) {
-    char lettre;
-
-    for (lettre = 'a'; lettre <= 'z'; lettre++) {
-        _putchar(lettre);
-    }
-    _putchar('\n');
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void) {
+    print_alphabet();
+    return (0);
 }
