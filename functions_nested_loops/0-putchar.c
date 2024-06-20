84b4c0277b9371e00fff85856cb3d7498c4c0c62
Zetitheuh <zetithondev@gmail.com>
2024-06-20 14:53:33 +0200

    e

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,11 +2,19 @@
 #include <main.h>
 /**
  * main - Entry of prog
- *
+ * write putchar using fonction from main.h
  * Return: Zero = program well executed
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');	
 	return(0);
 }
