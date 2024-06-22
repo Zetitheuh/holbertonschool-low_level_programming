f34acc1bd9970de61c247a7d7209f6a1d40b941d
Zetitheuh <zetithondev@gmail.com>
2024-06-23 00:39:06 +0200

    sd

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add tzwo integer
+ * @a: First integer
+ * @b: second one
+ * Return: Return the addition of them
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
