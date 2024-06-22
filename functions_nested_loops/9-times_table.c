60ca814f3ba2b2ae57deffc65e3d5db7e924bf9c
Zetitheuh <zetithondev@gmail.com>
2024-06-23 01:26:59 +0200

    m

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - print table
+ * Description: print the 9 times start to 0
+ */
+void times_table(void)
+{
+	int ligne = 10;
+	int colonne = 10;
+	int i;
+	int j;
+	int produit;
+
+	for (i = 0; i < ligne; i++)
+	{
+		for (j = 0; j < ligne; j++)
+		{
+			produit = i * j;
+			if (produit >= 10)
+			{
+				putchar('0' + produit / 10);
+				putchar('0' + produit % 10);
+			}
+			else
+			{
+				putchar('0' + produit);
+			}
+			if (j < colonne - 1)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+	return (0);
+}
