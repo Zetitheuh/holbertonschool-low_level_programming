0b685b1854cf50ad52bc937c6c5bfa452b505e83
Zetitheuh <zetithondev@gmail.com>
2024-06-23 01:00:58 +0200

    as

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print to 98 max
+ * @n: the stater to 98
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n != 98)
+			{
+				_putchar(n);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(n);
+				_putchar('\n');
+			}
+		}
+	}
+}
