8f33d9704e74e940a5594ff87b11980e6cdb1bce
Zetitheuh <zetithondev@gmail.com>
2024-06-22 20:00:59 +0200

    w

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - print + or - for n
+ * Description : s
+ * @n: the caracter to check
+ *
+ * Return: 1 if lower and 0 if not
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+	}
+	else if (n < 0 && n != 0)
+	{
+		_putchar('-');
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+}
