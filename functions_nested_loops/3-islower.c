e6e5bb7f5f23b00362d116dc275805732f397c4a
Zetitheuh <zetithondev@gmail.com>
2024-06-22 18:58:13 +0200

    s

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * _islower - prototype function
+ * Check if lowercase or not
+ *
+ * Return: 1 if lower and 0 if not
+ */
 int _islower(int c)
 {
     if (c >= 'a' && c <= 'z')
