e501b3cc668f8008aae8b040a4cbc38a3ffd3671
Zetitheuh <zetithondev@gmail.com>
2024-06-22 12:58:02 +0200

    s

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,17 +2,18 @@
 
 void print_alphabet_x10(void)
 {
-    int compteur = 0;
+	int compteur = 0;
 
-    while (compteur < 10)
-    {
-        char letter;
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-            _putchar(letter);
-        }
-        _putchar('\n');
-        compteur++;
-    }
+	while (compteur < 10)
+	{
+		char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+		_putchar('\n');
+		compteur++;
+	}
 }
 
