894515531a0d52b6acbf7a885855a63cbca78328
Zetitheuh <zetithondev@gmail.com>
2024-06-22 21:43:34 +0200

    s

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * jack_bauer - print minute
+ * print_jack_bauer_day - print minute
  * Description: print minute from 00 to 23:59
  */
-void print_jack_bauer_day()
+void print_jack_bauer_day(void)
 {
-    for (int hour = 0; hour < 24; hour++)
+	for (int hour = 0; hour < 24; hour++)
 	{
 	for (int minute = 0; minute < 60; minute++)
 	{
