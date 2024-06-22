a086f1296de583b914f99df39c0cc429493add1a
Zetitheuh <zetithondev@gmail.com>
2024-06-23 01:07:29 +0200

    s

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,13 +12,13 @@ void print_to_98(int n)
 		{
 			if (n != 98)
 			{
-				_putchar(n);
+				_putchar(n + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
 			else
 			{
-				_putchar(n);
+				_putchar(n + '0');
 				_putchar(',');
 				_putchar('\n');
 			}
