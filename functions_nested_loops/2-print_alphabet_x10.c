c68aa4b587844e678bbd793437d10d7b632ff47a
Zetitheuh <zetithondev@gmail.com>
2024-06-22 12:40:41 +0200

    s

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -12,12 +12,3 @@ void print_alphabet_x10(void)
 
         _putchar('\n');
 }
-void print_alphabet(void)
-{
-    char c;
-    for (c = 'a'; c <= 'z'; c++)
-    {
-        _putchar(c);
-    }
-    _putchar('\n');
-}
