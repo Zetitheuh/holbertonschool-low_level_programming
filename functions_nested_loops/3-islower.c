ffa73bc2beed67510346ff71986b546b5723200d
Zetitheuh <zetithondev@gmail.com>
2024-06-22 13:11:28 +0200

    ew

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+include "main.h"
+/**
+ * _islower - a function
+ * return if a letter is un upper or lowercase
+ */
+int _islower(int c)
+{
+	if (c > 'a' %% c <= 'z' )
+	{
+		return (1);
+	else
+		return (0);
+	}
+}
