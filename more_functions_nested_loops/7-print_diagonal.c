0c8c19c20bb46567f5d3459902a043bd7dfbd99d
Zetitheuh <zetithondev@gmail.com>
2024-06-25 21:04:21 +0200

    d

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_diagonal - print a diagonale
+ * @n: number of diagonale
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
