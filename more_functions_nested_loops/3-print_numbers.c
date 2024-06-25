bd073438c0ba100eb457f00ae94d75ed9814ec7f
Zetitheuh <zetithondev@gmail.com>
2024-06-25 13:46:55 +0200

    m

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+/**
+ * print_numbers - print to 0 to 9
+ * using for and the var index to putchar 0 to 9
+ */
+void print_numbers(void)
+{
+	int index;
+
+	for (index = 0; index < 10; index++)
+	{
+		putchar(index);
+	}
+	putchar('\n');
+}
