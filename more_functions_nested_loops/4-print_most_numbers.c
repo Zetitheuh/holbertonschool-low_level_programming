eaf20cbf09c0a097bb9c0b3ea19412c413daaade
Zetitheuh <zetithondev@gmail.com>
2024-06-25 13:58:02 +0200

    d

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - print to 0 to 9
+ * using for and the var index to putchar 0 to 9 exept 2 & 4
+ */
+void print_most_numbers(void)
+{
+        int index;
+
+        for (index = 0; index < 10; index++ && index != 2 && index != 4) 
+        {
+                _putchar(index + '0');
+        }
+        _putchar('\n');
+}
+~ 
