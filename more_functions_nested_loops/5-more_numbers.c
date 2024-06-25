ff98d2195bd4dbfbcbb902b91f39f1c9f4dcdce6
Zetitheuh <zetithondev@gmail.com>
2024-06-25 15:22:20 +0200

    s

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - print number 0 t0 14
+ */
+void more_numbers(void)
+{
+	int i;
+	int b;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+		if (b < 10)
+		{
+			_putchar(b + '0');
+		}
+		else
+		{
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+		}
+		}
+	putchar('\n');
+	}
+}
