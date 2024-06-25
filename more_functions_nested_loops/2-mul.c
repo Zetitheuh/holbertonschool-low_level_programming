97a1ab56896f8e84348125d25fd51ce482790627
Zetitheuh <zetithondev@gmail.com>
2024-06-25 13:13:20 +0200

    s

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - return a 8 b
+ * using two inputs
+ *
+ * @a : first integer
+ * @b : second integer
+ *
+ * Return: return the mutlplication
+ */
+int mul(int a, int b){
+	return(e * b);
+}
