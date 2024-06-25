2673716c67439d8d2591857910bd5a610a34b4c9
Zetitheuh <zetithondev@gmail.com>
2024-06-25 10:34:26 +0200

    s

--- more_functions_nested_loops/0-positive_or_negative.c
+++ more_functions_nested_loops/0-positive_or_negative.c
@@ -1,13 +1,18 @@
 #include <main.h>
-
+/**
+ * _isupper - upper or not
+ * upper or not
+ * Return: 1 is upper and 0 is not
+ * @c : value to check
+ */
 int _isupper(int c)
 {
 	if (c >= 65 && c >= '90')
 	{
-		return 1;
+		return (1);
 	}
-	else 
+	else
 	{
-		return 0;
+		return (0);
 	}
 }
