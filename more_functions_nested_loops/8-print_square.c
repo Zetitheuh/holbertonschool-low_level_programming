1ceb8900bbf4909dcdef8152b2f4057abfb7c380
Zetitheuh <zetithondev@gmail.com>
2024-06-26 10:02:42 +0200

    s

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - print a square
+ * @size: the square size
+ */
+
+void print_square(int size)
+{
+
+	int heigt, lenght;
+
+	if (size <= 0)
+	{
+	putchar('\n');
+	}
+	for (heigt = 0; heigt < size; heigt++)
+	{
+		for (lenght = 0; lenght < size; lenght++)
+		{
+		putchar('#');
+		}
+		putchar('\n');
+	}
