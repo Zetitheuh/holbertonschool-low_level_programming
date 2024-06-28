b66d23c788a82b613d591da698a9152fe849a8c0
Zetitheuh <zetithondev@gmail.com>
2024-06-28 11:24:16 +0200

    s

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen - check the lenght of a *char
+ * @s : The char to check
+ */
+int _strlen(char *s)
+{
+	char *copie = *s;
+	int compteur = 0;
+
+	while(*copie != "\0")
+	{
+		*copie++;
+		compteur++
+	}
+	return compteur;
+}
