6cd7a53a88af9eda63ac17ba696c2f5bbc0ae888
Zetitheuh <zetithondev@gmail.com>
2024-06-28 14:34:00 +0200

    sa

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+/**
+ * print_rev - print in reverse
+ * @s : the string to reverse
+ */
+void print_rev(char s)
+{
+	premier_char = *s
+	while(*s != '\0')
+	{	
+		s++
+	}
+	while(*s !+ premier_char)
+	{
+		printf("%c", *premier_char)
+		premier_char--;
+	}
+
+}
