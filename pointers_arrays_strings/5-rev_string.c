528f292297e01eff9c539005b0b5051a3078ea92
Zetitheuh <zetithondev@gmail.com>
2024-06-28 21:01:04 +0200

    s

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,18 +1,20 @@
 /**
  * rev_string - reverse the string
- * @s a long *char
+ * @s: a long *char
  */
 void rev_string(char *s)
 {
+	char temp;
 	int counter = 0;
+	int i;
 
 	while (s[counter] != '\0')
-	{
+        {
 		counter++;
 	}
-	while (counter != 0)
-	{
-		counter--;
-		*s = s[counter];
+	for (i = 0; i < counter / 2; i++) {
+		temp = s[i];
+		s[i] = s[counter - 1 - i];
+		s[counter - 1 - i] = temp;
 	}
 }
