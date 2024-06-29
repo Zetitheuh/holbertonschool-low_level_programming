4e7231739bed1bd81d08d5806d4995829ab25bc2
Zetitheuh <zetithondev@gmail.com>
2024-06-29 13:18:36 +0200

    s

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - print 1/2 caractere
+ * @str : the string to use
+ */
+void puts2(char *str)
+{
+	int counter = 0;
+
+	while(str[counter] != '\0')
+	{
+		_putchar(str[counter]);
+		counter++;
+		counter++;
+	}
+}
