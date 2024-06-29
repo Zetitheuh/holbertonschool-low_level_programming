99be6c7f0abb7501788c0a3d97a467f5d402e9a6
Zetitheuh <zetithondev@gmail.com>
2024-06-29 14:44:45 +0200

    s

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,25 +1,32 @@
+/**
+ * puts_half - print alph of a char
+ * @str : the char
+ */
 void puts_half(char *str)
 {   
-    char *copie = str;
-    int counter = 0;
-    int counter2 = 0;
-    while (copie[counter] != '\0')
-    {
-        counter++;
-    }   if (counter % 2 == 0)
-        {
-            for (int i = 0; i < counter / 2; i++)
-            {
-                _putchar(str[i]);
-            }
-        }
-        else
-        {
-            for (int i = 0; i < counter / 2; i++)
-            {
-                _putchar(str[i]);
-                i++;
-                 _putchar(str[i]);
-            }
-        }
+	char *copie = str;
+	int counter = 0;
+	int counter2 = 0;
+	int i = 0;
+    
+	while (copie[counter] != '\0')
+	{
+		counter++;
+	}   
+	if (counter % 2 == 0)
+	{
+		for (i = 0; i < counter / 2; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+		else
+		{
+		for (i = 0; i < counter / 2; i++)
+		{
+			_putchar(str[i]);
+			i++;
+			 _putchar(str[i]);
+		}
+	}
 }
