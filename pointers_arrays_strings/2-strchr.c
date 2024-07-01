af3913511276317790fccffa256c95cd5608c228
Zetitheuh <zetithondev@gmail.com>
2024-07-01 13:39:42 +0200

    s

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+/**
+ * _strchr - print a pointer started by letter c
+ * @s: the char pointer
+ * @c: the letter where we start to print the pointer
+ *
+ * Return: The string started by c or null if c not in *s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (NULL);
+}
