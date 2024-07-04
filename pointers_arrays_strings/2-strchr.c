5c330452687613bc430aca806245654fa4dbbc40
Zetitheuh <zetithondev@gmail.com>
2024-07-04 15:51:01 +0200

    s

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,19 +7,18 @@
  * Return: The string started by c or null if c not in *s
  */
 char *_strchr(char *s, char c)
-{
+	{
 	while (*s != '\0')
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
+	if (*s == c)
+	{
+		return (s);
+	}
+	s++;
 	}
 	if (*s == '\0')
 	{
-		return (NULL);
+		return (0);
 	}
-
 	return (s);
 }
