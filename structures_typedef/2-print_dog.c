254768ba34d767da21c2b8d6c3844cf2935fc9d1
Zetitheuh <zetithondev@gmail.com>
2024-07-12 12:33:26 +0200

    fix the code

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+print_dog - Print all element of a dog
+ @d: pointer to struct dog
+ Return: void*/
+void print_dog(struct dog *d)
+{
+    printf("%s\n", my_dog.name);
+    printf("%f\n", my_dog.age);
+    printf("%s\n", my_dog.owner);
+}
