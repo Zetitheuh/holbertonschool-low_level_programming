ee05a21efc8d7b8c5998a60da2206ce1ac0e8b34
Zetitheuh <zetithondev@gmail.com>
2024-07-12 12:15:32 +0200

    fix

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,8 +1,21 @@
+#include "dog.h"
 #include <stdlib.h>
 #include <string.h>
-#include "dog.h"
-void init_dog(struct dog *d, char *name, float age, char *owner){
-    d->name = name;
-    d->age = age;
-    d->owner = owner; 
+/**
+ * init_dog - take data and initialise a structure dog with name/age/owner
+ * @d: pointer to struct dog
+ * @name: name of the dog
+ * @age:  age of the dog
+ * @owner: name of the owner
+ * Description: take de caracteristiques of the dog then change it
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
