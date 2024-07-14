91e2c09ae1c4e692c4bfd8017b5a280354aa1ac4
Zetitheuh <zetithondev@gmail.com>
2024-07-14 11:37:58 +0200

    add dog_t

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -36,3 +36,13 @@ void print_dog(struct dog *d)
 		printf("Owner: %s\n", d->owner);
 	}
 }
+int main(void)
+{
+    struct dog my_dog;
+
+    my_dog.name = "Poppy";
+    my_dog.age = 3.5;
+    my_dog.owner = "Bob";
+    print_dog(&my_dog);
+    return 0;
+}
